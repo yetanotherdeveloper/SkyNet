c9e9764e5235e807507b29f6bde4268bb293a214
jczaja <jczaja@localhost.localdomain>
2014-01-11 19:13:18 +0100

    - Added creation/building of Program,Kernel for PLA

--- code/modules/pla/pla.cpp
+++ code/modules/pla/pla.cpp
@@ -1,44 +1,84 @@
 #include "pla.h"
 #include <CL/cl.hpp>
 
+static std::string kernelSource = "__kernel void dodaj(__global float4 veciu) \
+                                   {  \
+                                            veciu = (float4)(1.0f,1.0f,1.0f,1.0f); \
+                                   }";
+
 extern "C" ISkyNetClassificationProtocol* CreateModule(const cl::Device* const pdevice)
 {
     return new PerceptronLearningAlgorithm(pdevice);
 }
 
 /*! Build kernels , initialize data
- *  
-*/
+ *
+ */
 PerceptronLearningAlgorithm::PerceptronLearningAlgorithm(const cl::Device* const pdevice) : m_about("Perceptron Learning Algorithm"), m_pdevice(pdevice)
 {
     cl_int err;
 
-   // TODO:
-   // - create command queue
-   // - build programs, make kernels
-   // - store device, command queue, context
+    // TODO:
+    // - create command queue
+    // - build programs, make kernels
+    // - store device, command queue, context
 
 
     // make a context , command queue
-    m_context = new cl::Context(*m_pdevice,NULL,NULL,NULL,&err); 
+    m_context = new cl::Context(*m_pdevice,NULL,NULL,NULL,&err);
 
-    if( err != CL_SUCCESS ) {
+    if( err != CL_SUCCESS )
+    {
         //SKYNET_INFO(" Error creating OpenCL context: %d\n",err);
         printf("Error creating OpenCL context for PLA module: %d\n",err);
     }
-    
+
     std::vector<cl::Device> context_devices;
     m_context->getInfo(CL_CONTEXT_DEVICES,&context_devices);
 
-    m_queue = new cl::CommandQueue(*m_context,*m_pdevice,0,&err); 
+    m_queue = new cl::CommandQueue(*m_context,*m_pdevice,0,&err);
 
-    if( err != CL_SUCCESS ) {
+    if( err != CL_SUCCESS )
+    {
         //SKYNET_INFO(" Error creating OpenCL command queue: %d\n",err);
         printf(" Error creating OpenCL command queue: %d\n",err);
     }
+
+    // Create/build program, get kernels
+
+    cl::Program::Sources kern_sources(1, std::make_pair(kernelSource.c_str(), kernelSource.length() + 1));
+    cl::Program program(*m_context, kern_sources,&err);
+    if( err != CL_SUCCESS )
+    {
+        //SKYNET_INFO(" Error creating OpenCL command queue: %d\n",err);
+        printf(" Error creating OpenCL program from source: %d\n",err);
+    }
+
+    std::vector<cl::Device> targetDevices(1,*m_pdevice);
+    err = program.build(targetDevices, "-cl-std=CL1.0");
+    if( err != CL_SUCCESS )
+    {
+        //SKYNET_INFO(" Error creating OpenCL command queue: %d\n",err);
+        printf(" Error Building OpenCL program failed: %d\n",err);
+        std::string log; 
+        err =  program.getBuildInfo(*m_pdevice,CL_PROGRAM_BUILD_LOG,&log);
+        printf(" OpenCL program build log: %s\n",log.c_str());
+    }
+
+    m_plaKernel = new cl::Kernel(program, "dodaj", &err);
+    if( err != CL_SUCCESS )
+    {
+        //SKYNET_INFO(" Error creating OpenCL command queue: %d\n",err);
+        printf(" Error creating OpenCL PLA kernel : %d\n",err);
+    }
+
+
+    //TODO: error handling section
+
 }
 
-void PerceptronLearningAlgorithm::Run(){
+void PerceptronLearningAlgorithm::Run()
+{
 }
 
 const std::string PerceptronLearningAlgorithm::About() const
@@ -50,6 +90,6 @@ PerceptronLearningAlgorithm::~PerceptronLearningAlgorithm()
 {
     delete m_context;
     m_context = NULL;
-    delete m_queue; 
+    delete m_queue;
     m_queue = NULL;
 }
--- code/modules/pla/pla.h
+++ code/modules/pla/pla.h
@@ -8,6 +8,7 @@ namespace cl
     class Device;
     class Context;
     class CommandQueue;
+    class Kernel;
 }
 
 class PerceptronLearningAlgorithm : public ISkyNetClassificationProtocol
@@ -16,6 +17,7 @@ class PerceptronLearningAlgorithm : public ISkyNetClassificationProtocol
         std::string m_about;
         cl::Context *m_context;
         cl::CommandQueue *m_queue;
+        cl::Kernel* m_plaKernel;
         const cl::Device *const m_pdevice;
     public:
         PerceptronLearningAlgorithm(const cl::Device* const pdevice);
