d5f8d86dddec88f682d19b082e9787a9a1be8116
jacek.czaja <jczaja@localhost.localdomain>
2014-06-05 23:48:12 +0200

    - Added draft of termination criteria for sgd

--- code/modules/sgd/sgd.cpp
+++ code/modules/sgd/sgd.cpp
@@ -15,7 +15,8 @@ extern "C" ISkyNetClassificationProtocol* CreateModule(const cl::Device* const p
 /*! Build kernels , initialize data
  *
  */
-StochasticGradientDescent::StochasticGradientDescent(const cl::Device* const pdevice) : m_about(StochasticGradientDescent::composeAboutString(pdevice) ), m_pdevice(pdevice), m_theta(0.1) 
+StochasticGradientDescent::StochasticGradientDescent(const cl::Device* const pdevice) : m_about(StochasticGradientDescent::composeAboutString(pdevice) ), m_pdevice(pdevice),
+                                                     m_theta(0.1), m_flatness(0.0000001f)
 {
     cl_int err;
 
@@ -62,9 +63,24 @@ bool StochasticGradientDescent::updateWeights(const point &randomSample)
 {
     // TODO: make it working for any dimentions not just two
     // Perform gradient descent on given (assuming random) sample
-    m_weights[0] += -m_theta*2.0*(randomSample.x * m_weights[1] + m_weights[2] * randomSample.y + m_weights[0] - randomSample.classification);                // gradient per w0
-    m_weights[1] += -m_theta*2.0*(randomSample.x * m_weights[1] + m_weights[2] * randomSample.y + m_weights[0] - randomSample.classification)*randomSample.x; // gradient per w1
-    m_weights[2] += -m_theta*2.0*(randomSample.x * m_weights[1] + m_weights[2] * randomSample.y + m_weights[0] - randomSample.classification)*randomSample.y; // gradient per w2
+    float dw0,dw1,dw2;
+    dw0 = -m_theta*2.0*(randomSample.x * m_weights[1] + m_weights[2] * randomSample.y + m_weights[0] - randomSample.classification);                // gradient per w0
+    dw1 = dw0*randomSample.x; // gradient per w1
+    dw2 = dw0*randomSample.y; // gradient per w2
+
+    m_weights[0] += dw0;
+    m_weights[1] += dw1;
+    m_weights[2] += dw2;
+
+    // sum of updates to weights is less then our flatness value then
+    // we decalre that no progress is made
+    if(dw0*dw0 + dw1*dw1 + dw2*dw2 <= m_flatness) {
+        return true;
+    }
+
+    //m_weights[0] += -m_theta*2.0*(randomSample.x * m_weights[1] + m_weights[2] * randomSample.y + m_weights[0] - randomSample.classification);                // gradient per w0
+    //m_weights[1] += -m_theta*2.0*(randomSample.x * m_weights[1] + m_weights[2] * randomSample.y + m_weights[0] - randomSample.classification)*randomSample.x; // gradient per w1
+    //m_weights[2] += -m_theta*2.0*(randomSample.x * m_weights[1] + m_weights[2] * randomSample.y + m_weights[0] - randomSample.classification)*randomSample.y; // gradient per w2
     return false;
 }
 
@@ -85,7 +101,11 @@ const std::vector<float> & StochasticGradientDescent::RunRef(const std::vector<p
         diagnostic.storeWeights(m_weights);
         if(finish == false) {
             ++i;
+        } else {
+            // TODO: If flatness was reached then
+            // check if error is below certain error threshold 
         }
+
     } 
     if(finish == false) {
         printf("Warning: Stochastic Gradient Descent Learning alogorithm exhusted all iterations. TODO: Make proper termination criteria\n");
